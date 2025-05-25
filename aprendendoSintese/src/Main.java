b8c3a24e47980245a251db1e7ff19c306e9ddcbc
Kayo Weiber <caioveiber598@gmail.com>
2025-05-25 12:07:06 -0300

    build(studing Java)
    - Learning Java Synthesis

---
+++ aprendendoSintese/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        byte b=100;
+        short s=10000;
+        int i=100000;
+        long l=100000L;
+        float f=10.5f;
+        double d=20.5;
+        char c='A';
+        String str="Kayo";
+        boolean boll=true;
+
+        if(boll==true){
+            System.out.println("KayoLindo");
+        }else{
+            System.out.println("Falso");
+        }
+    }
+}
